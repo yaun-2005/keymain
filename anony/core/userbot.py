eee9b0399dffdcfeed28f769e786a1cfa6478595
YanYan <yaunyaun2026@gmail.com>
2026-08-23 20:54:59 +0630

    Dev999
    Signed-off-by: YanYan <yaunyaun2026@gmail.com>

--- anony/core/userbot.py
+++ anony/core/userbot.py
@@ -49,7 +49,7 @@ class Userbot(Client):
         client = clients[num]
         await client.start()
         try:
-            await client.send_message(config.LOGGER_ID, "Assistant Now Ready 999cores_❍ᴡηєʀ")
+            await client.send_message(config.LOGGER_ID, "Assistant Now Ready YanYan_❍ᴡηєʀ")
         except Exception:
             raise SystemExit(f"Assistant {num} failed to send message in log group.")
 
