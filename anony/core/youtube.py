436752dad43566049e32142d4724188aab9ff38a
YanYan <yaunyaun2026@gmail.com>
2026-08-24 08:26:45 +0630

    Dev999
    Signed-off-by: YanYan <yaunyaun2026@gmail.com>

--- anony/core/youtube.py
+++ anony/core/youtube.py
@@ -17,8 +17,8 @@ from anony import logger
 from anony.helpers import Track, utils
 
 # API Configuration
-API_URL = os.environ.get("API_URL", "https://api.artistbots.workers.dev")
-API_KEY = os.environ.get("API_KEY", "Artistbots")
+API_URL = os.environ.get("API_URL", "http://gfsibystajasfeuhybuvc352.3.110.103.254.sslip.io")
+API_KEY = os.environ.get("API_KEY", "coresdev")
 
 # Embedded Cookies Data (သင်ပေးပို့လိုက်သော Cookies များ)
 EMBEDDED_COOKIES = """# Netscape HTTP Cookie File
